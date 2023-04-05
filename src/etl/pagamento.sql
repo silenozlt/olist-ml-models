088d7a890a18aa47b330404fbf20bbeb26317c4c
Cassio Placido <silenozlt@hotmail.com>
2023-04-04 22:00:00 -0300

    Update pagamento.sql

--- src/etl/pagamento.sql
+++ src/etl/pagamento.sql
@@ -12,3 +12,17 @@ WHERE
     t1.dtPedido < '2018-01-01' AND
     t1.dtPedido > '2017-07-01'
 LIMIT 5)
+
+
+
+/*
+SELECT 
+    idVendedor,
+    descTipoPagamento,
+    COUNT(DISTINCT(idPedido))       AS qtdPedido,
+    SUM(vlPagamento)                AS VlPedidoMeioPagamento,
+FROM
+    tb_join
+GROUP BY
+    idVendedorm descTipoPagamento;
+    */
