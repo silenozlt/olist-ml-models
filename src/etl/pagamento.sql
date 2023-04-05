1dda51b2953a799d11df86cb9e1989ab93e85e7a
Cassio Placido <silenozlt@hotmail.com>
2023-04-04 21:59:39 -0300

    Day 2
    Segundo dia

---
+++ src/etl/pagamento.sql
@@ -0,0 +1,14 @@
+WITH tb_join AS (
+SELECT 
+    t2.*,
+    t3.idVendedor
+FROM 
+    Pedido t1
+LEFT JOIN
+    pagamento_pedido t2 ON (t1.idPedido = t2.idPedido)
+LEFT JOIN
+    item_pedido t3 ON (t1.idPedido = t2.idPedido)
+WHERE
+    t1.dtPedido < '2018-01-01' AND
+    t1.dtPedido > '2017-07-01'
+LIMIT 5)
